567ec38a96d80cfc059b1ed2b25e6d08d5c0d640
MRtecno98 <mr.tecno98@gmail.com>
2022-06-13 14:08:41 +0200

    Add url resolver logic

---
+++ resolver/resolver.go
@@ -0,0 +1,32 @@
+package resolver
+
+import (
+	"errors"
+	"net/url"
+
+	"github.com/spf13/afero"
+	"github.com/spf13/afero/sftpfs"
+)
+
+var protocols = map[string]func(*url.URL) (afero.Fs, error){
+	"mem": func(*url.URL) (afero.Fs, error) {
+		return afero.NewMemMapFs(), nil
+	},
+
+	"sftp": sftpfs.Resolve,
+	"ssh":  sftpfs.Resolve,
+}
+
+func OpenUrl(u string) (afero.Fs, error) {
+	urlparse, err := url.Parse(u)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if proto, ok := protocols[urlparse.Scheme]; ok {
+		return proto(urlparse)
+	} else {
+		return nil, errors.New("protocol not implemented")
+	}
+}
